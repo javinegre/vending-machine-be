62689902d826d3af0432a4798dda4a1c66581ef9
Javier Negre <javi.negre@gmail.com>
2023-09-01 12:03:07 +0200

    First endpoint healthcheck

--- vending_machine_be/urls.py
+++ vending_machine_be/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.health.views import healthcheck
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('healthcheck/', healthcheck)
 ]
