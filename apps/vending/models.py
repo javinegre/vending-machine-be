a641bce119d047e3bb44008cab4a89cfb1a6b7c3
Javier Negre <javi.negre@gmail.com>
2023-09-01 17:03:58 +0200

    Update models: Product and VendingMachineSlot (M3-S4)

--- apps/vending/models.py
+++ apps/vending/models.py
@@ -13,6 +13,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=4, decimal_places=2, validators=[
                                 MinValueValidator(Decimal("0.00"))])
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
 
 
 class VendingMachineSlot(models.Model):
