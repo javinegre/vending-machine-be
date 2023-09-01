92318408c230dbedcd4e88b231eed5c6c860081e
Javier Negre <javi.negre@gmail.com>
2023-09-01 16:59:34 +0200

    Add relationships between models (M3-S3)

--- apps/vending/models.py
+++ apps/vending/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from decimal import Decimal
 from uuid import uuid4
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Product(models.Model):
@@ -13,3 +13,17 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=4, decimal_places=2, validators=[
                                 MinValueValidator(Decimal("0.00"))])
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class VendingMachineSlot(models.Model):
+    class Meta:
+        db_table = "vending_machine_slot"
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+    quantity = models.IntegerField(
+        validators=[MaxValueValidator(100), MinValueValidator(0)])
+    row = models.IntegerField(
+        validators=[MaxValueValidator(10), MinValueValidator(1)])
+    column = models.IntegerField(
+        validators=[MaxValueValidator(10), MinValueValidator(1)])
