796de18ef1ff83865458db7786e395763d4072f4
Javier Negre <javi.negre@gmail.com>
2023-09-02 16:03:58 +0200

    Configure Admin (M5-S2, M5-S3)

--- apps/vending/models.py
+++ apps/vending/models.py
@@ -8,6 +8,9 @@ class Product(models.Model):
     class Meta:
         db_table = "product"
 
+    def __str__(self):
+        return "{0} (${1})".format(self.name, self.price)
+
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=4, decimal_places=2, validators=[
@@ -19,6 +22,9 @@ class Product(models.Model):
 class VendingMachineSlot(models.Model):
     class Meta:
         db_table = "vending_machine_slot"
+
+    def __str__(self):
+        return "{0}-{1} > {2}".format(self.row, self.column, str(self.product))
 
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     product = models.ForeignKey("Product", on_delete=models.CASCADE)
