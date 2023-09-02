7947cdbc46394534f157c005b1827a0f775e719b
Javier Negre <javi.negre@gmail.com>
2023-09-02 19:51:52 +0200

    Include a serialized response for slots (M6-S2.2)

--- apps/vending/views.py
+++ apps/vending/views.py
@@ -2,7 +2,12 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.views import APIView
 
+from apps.vending.models import VendingMachineSlot
+from apps.vending.serializers import VendingMachineSlotSerializer
+
 
 class VendingMachineSlotView(APIView):
     def get(self, request: Request) -> Response:
-        return Response()
+        slots = VendingMachineSlot.objects.all()
+        slots_serializer = VendingMachineSlotSerializer(slots, many=True)
+        return Response(data=slots_serializer.data)
