df808563540a0072cffbdc53693392bd9e9e9623
Javier Negre <javi.negre@gmail.com>
2023-09-03 13:34:19 +0200

    Add missing file in previous commit

---
+++ apps/vending/validators.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+
+class ListSlotsValidator(serializers.Serializer):
+    quantity = serializers.IntegerField(
+        required=False, min_value=0, default=None)
